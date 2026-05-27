d1fcd1cc4d951092e8685aa4a9025e15e91095d0
Neo-PC <galinabunu26@gmail.com>
2026-05-27 15:25:13 +0200

    Initial commit

---
+++ proiectegit/repo-payment/src/main/java/com/payment/repo_payment/RepoPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.payment.repo_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoPaymentApplication.class, args);
+	}
+
+}
---
+++ proiectegit/repo-payment/src/test/java/com/payment/repo_payment/RepoPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.repo_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
